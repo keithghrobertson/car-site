fbe28b222683d4401b822e44c025707819d63ec7
Keith Robertson <keithghrobertson@gmail.com>
2014-05-06 18:58:32 -0700

    Added a navigation menu, and have a parallex scroll effect on the header image

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
 $(document).ready(function() {
 	divOneResize();
 
-});
+	//clicking down arrow brings you past the fold
+	$('#scroll-down').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $('#two').offset().top
+		}, 1000);
+	});
+	
+	var navOpen = false;
+	
+	$('#nav-button').click(function(){
+		if(navOpen == false){
+			$("#nav").animate({left: '0'},{speed : 2000, easing: 'easeInOutCirc'});
+			$("#page-wrapper").animate({left: '260px'},{speed : 2000, easing: 'easeInOutCirc'});
+			navOpen = true;
+		}else{
+			$("#nav").animate({left: '-260px'},{speed : 200, easing: 'easeInOutCirc'});
+			$("#page-wrapper").animate({left: '0px'},{speed : 2000, easing: 'easeInOutCirc'});
+			navOpen = false;
+		}
+	});//end click
+	
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+     
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+             
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+	
+	
+});//end .ready
+
 
 // for the window resize
 $(window).resize(function() {
